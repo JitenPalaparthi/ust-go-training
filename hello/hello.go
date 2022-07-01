283d5be3057673ccafe4c3d3a98c83edf6c5e067
Jiten Palaparthi <JPalaparthi@vmware.com>
2022-07-01 09:36:33 +0530

    Add hello world

---
+++ hello/hello.go
@@ -0,0 +1,18 @@
+package main
+
+func main() {
+	println("Hello World")
+}
+
+// There are few built in functions in go --> println
+// go run main.go
+// go run --work main.go
+// go tool compile main.go
+// go tool compile -S main.go
+
+// go build main.go
+// go build -o app main.go
+// GOARCH=amd64 GOOS=windows go build main.go
+// go tool dist list
+// go install main.go
+// go install github.com/cweill/gotests/gotests@latest
